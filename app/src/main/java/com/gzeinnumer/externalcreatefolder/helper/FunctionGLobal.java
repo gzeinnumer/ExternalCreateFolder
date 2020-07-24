2ce1ee270fae8410cf5f71fb6d931c558f2594ae
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-07-25 02:07:11 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/externalcreatefolder/helper/FunctionGLobal.java
@@ -0,0 +1,54 @@
+package com.gzeinnumer.externalcreatefolder.helper;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+public class FunctionGLobal {
+
+    public static String getStorageCard = Environment.getExternalStorageDirectory().toString();
+    public static String appFolder = "/ExternalCreateFolder";
+
+    private static final String TAG = "FunctionGLobal_";
+
+    public static void myLogD(String tag,String msg){
+        Log.d("MyZein", tag+"_"+msg);
+    }
+
+    public static boolean initFolder() {
+        File folder;
+
+        // create folder
+        folder = new File(getStorageCard + appFolder);
+        if (!folder.exists()) {
+            if (!creatingFolder(folder)){
+                return false;
+            }
+        }
+        folder = new File(getStorageCard + appFolder + "/db");
+        if (!folder.exists()) {
+            if (!creatingFolder(folder)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean creatingFolder(File folder){
+        try{
+            if (folder.mkdirs()){
+                myLogD(TAG, "Folder created");
+            }
+        } catch (Exception e){
+            myLogD(TAG, "Folder not created");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isFileExists(String path){
+        File file = new File(getStorageCard + path);
+        return file.exists();
+    }
+}
